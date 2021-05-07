1453535a76897536b06f718031fc5c214d5540f3
fmmartian <62557937+fmmartian@users.noreply.github.com>
2021-05-07 10:27:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,5 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/liuran001/openwrt-packages.git packages/luci-app-adguardhome
-git clone https://github.com/liuran001/openwrt-packages.git packages/luci-app-unblockmusic
-git clone https://github.com/liuran001/openwrt-packages.git packages/UnblockNeteaseMusic
-git clone https://github.com/liuran001/openwrt-packages.git packages/UnblockNeteaseMusicGo
+git clone https://github.com/liuran001/openwrt-packages.git package/luci-app-adguardhome
+git clone https://github.com/liuran001/openwrt-packages.git package/luci-app-aliddns
